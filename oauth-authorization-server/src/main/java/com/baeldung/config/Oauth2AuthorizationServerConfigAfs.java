b498ccf0423f45b59319620fd5217bbbe8a47055
Francesc Gonzalez <francesc.gonzalez@tipico.com>
2019-08-23 11:33:26 +0200

    Added own authentication module

--- oauth-authorization-server/src/main/java/com/baeldung/config/Oauth2AuthorizationServerConfigAfs.java
+++ oauth-authorization-server/src/main/java/com/baeldung/config/Oauth2AuthorizationServerConfigAfs.java
@@ -1,7 +1,5 @@
 package com.baeldung.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -21,30 +19,25 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-//@Configuration
-//@EnableAuthorizationServer
-public class OAuth2AuthorizationServerConfigJwt extends AuthorizationServerConfigurerAdapter {
+import java.util.Arrays;
+
+@Configuration
+@EnableAuthorizationServer
+public class Oauth2AuthorizationServerConfigAfs extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
     @Override
-    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
     @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception { // @formatter:off
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("sampleClientId")
-                .authorizedGrantTypes("implicit")
-                .scopes("read", "write", "foo", "bar")
-                .autoApprove(true)
-                .accessTokenValiditySeconds(3600)
-                .redirectUris("http://localhost:8083/","http://localhost:8086/")
 
-                .and()
                 .withClient("fooClientIdPassword")
                 .secret(passwordEncoder().encode("secret"))
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials")
@@ -52,23 +45,12 @@ public class OAuth2AuthorizationServerConfigJwt extends AuthorizationServerConfi
                 .autoApprove(true)
                 .accessTokenValiditySeconds(3600)       // 1 hour
                 .refreshTokenValiditySeconds(2592000)  // 30 days
-                .redirectUris("http://www.example.com","http://localhost:8089/","http://localhost:8080/login/oauth2/code/custom","http://localhost:8080/ui-thymeleaf/login/oauth2/code/custom", "http://localhost:8080/authorize/oauth2/code/bael", "http://localhost:8080/login/oauth2/code/bael")
-
-                .and()
-                .withClient("barClientIdPassword")
-                .secret(passwordEncoder().encode("secret"))
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .scopes("bar", "read", "write")
-                .accessTokenValiditySeconds(3600)       // 1 hour
-                .refreshTokenValiditySeconds(2592000)  // 30 days
-
-                .and()
-                .withClient("testImplicitClientId")
-                .authorizedGrantTypes("implicit")
-                .scopes("read", "write", "foo", "bar")
-                .autoApprove(true)
-                .redirectUris("http://www.example.com");
-    } // @formatter:on
+                .redirectUris("http://www.example.com","http://localhost:8089/",
+                        "http://localhost:8080/login/oauth2/code/custom",
+                        "http://localhost:8080/ui-thymeleaf/login/oauth2/code/custom",
+                        "http://localhost:8080/authorize/oauth2/code/bael",
+                        "http://localhost:8080/login/oauth2/code/bael");
+    }
 
     @Bean
     @Primary
